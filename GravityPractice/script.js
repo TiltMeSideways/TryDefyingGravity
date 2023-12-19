be897c1f8c9cb49aa08929400b20c82de18ae9c8
Kain Stewart <151081489+TiltMeSideways@users.noreply.github.com>
2023-12-19 21:04:58 +0000

    Update script.js
    tidied code, changed IF ELSE into ternary for beautifacation.

--- GravityPractice/script.js
+++ GravityPractice/script.js
@@ -11,29 +11,30 @@ document.addEventListener("DOMContentLoaded", function (){
     const gravity = 0.9;
     const bounceFactor = 1; 
 
-    function drawBall(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.arc(posX, posY, 20, 0, Math.PI * 2);
-    if(posX > canvas.width / 2) 
-    {
-        ctx.fillStyle = "blue";
-    }
-    else {
-        ctx.fillStyle = "red";
-    }
-    ctx.fill();
-    ctx.closePath();
+        function drawBall(){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.beginPath();
+            ctx.arc(posX, posY, 20, 0, Math.PI * 2);
+            ctx.fillStyle = posX > canvas.width / 2 ? "blue" : "red";
+            ctx.fill();
+            ctx.closePath();
 }
 
-    function applyGravity(){
-        velocityY += gravity;
-        posY += velocityY;
-        const floorLevel = canvas.height - 20;
+if(posX > canvas.width / 2) 
+{
+    ctx.fillStyle = "blue";
+}
+else {
+    ctx.fillStyle = "red";
+}
+        function applyGravity(){
+            velocityY += gravity;
+            posY += velocityY;
+            const floorLevel = canvas.height - 20;
 
-    if (posY > floorLevel) {
-        posY = floorLevel;
-        velocityY *= -bounceFactor;
+        if (posY > floorLevel) {
+            posY = floorLevel;
+            velocityY *= -bounceFactor;
     }
 }
 
@@ -42,31 +43,29 @@ document.addEventListener("DOMContentLoaded", function (){
         
         if(posX < 20 || posX > canvas.width - 20) {
             velocityX *= -bounceFactor;
-        }
     }
+}
 
     function animate(){
         applyGravity();
         horizontalMovement();
         drawBall();
         animationId = requestAnimationFrame(animate);
-    }
-
-function startAnimation(){
-    canvas.style.display = "block";
-    playButton.style.display = "none";
-    animate();
 }
 
-function stopAnimation(){
-    canvas.style.display = "none";
-    playButton.style.display = "block";
-    cancelAnimationFrame(animationId);
+    function startAnimation(){
+        canvas.style.display = "block";
+        playButton.style.display = "none";
+        animate();
 }
 
-document.getElementById("playButton").addEventListener("click", startAnimation);
-canvas.addEventListener("click",stopAnimation);
+    function stopAnimation(){
+        canvas.style.display = "none";
+        playButton.style.display = "block";
+        cancelAnimationFrame(animationId);
+}
 
-
+    document.getElementById("playButton").addEventListener("click", startAnimation);
+    canvas.addEventListener("click",stopAnimation);
 
 });
