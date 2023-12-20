4c62620ff6eebc7596ee8dc2316d1507cc1b3baf
Kain Stewart <151081489+TiltMeSideways@users.noreply.github.com>
2023-12-20 22:03:33 +0000

    Updated Javascript further.
    This update includes not only side to side arrow key movement of the ball, but two stepping stones for the ball to bounce on.
    
    I hope to, in the future, make the ball stop the animation and return to the play button upon the ball making contact with the floor. with maybe a game over alert message, or something along those lines.

--- GravityPractice/script.js
+++ GravityPractice/script.js
@@ -4,57 +4,126 @@ document.addEventListener("DOMContentLoaded", function (){
     const ctx = canvas.getContext("2d");
     let animationId;
 
-    let posX = 50;
     let posY = 50;
-    let velocityY = 0;
-    let velocityX = 5;
-    const gravity = 0.9;
-    const bounceFactor = 1; 
+    let posX = canvas.width/2;
+    let velocityY = 1;
+    let velocityX = 0;
+    const gravity = 0.5;
+    //bounce factor removed for keeping height and Y velocity.
+    const horizontalSpeed = 10;
+    const steppingStone = {
+        posX: canvas.width / 2 + 200,
+        posY: canvas.height - 50,
+        width: 50,
+        height: 10,
+};
 
-        function drawBall(){
+    function drawBall () {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
+            applyGravity(canvas.height - 20, gravity);
+            //Gavity only on this ball, apply to others if needed.
+            handleCollision();
             ctx.beginPath();
-            ctx.arc(posX, posY, 20, 0, Math.PI * 2);
+            ctx.arc(posX, posY, 20 , 0, Math.PI * 2);
             ctx.fillStyle = posX > canvas.width / 2 ? "blue" : "red";
             ctx.fill();
             ctx.closePath();
 }
 
-        function applyGravity(){
+    const applyGravity = (floorLevel, gravity) => {
             velocityY += gravity;
             posY += velocityY;
-            const floorLevel = canvas.height - 20;
+            //const floorLevel = canvas.height - 20;
 
         if (posY > floorLevel) {
             posY = floorLevel;
-            velocityY *= -bounceFactor;
+            velocityY = -velocityY;
+            //velocityY set to reverse so that the bounce maintains height
     }
 }
+
+    function drawSteppingStone() {
+        ctx.beginPath();
+        ctx.rect(steppingStone.posX, steppingStone.posY, steppingStone.width, steppingStone.height);
+        ctx.fillStyle = "blue";
+        ctx.fill();
+        ctx.closePath();
+}
+
+    function drawSteppingStone2() {
+        ctx.beginPath();
+        ctx.rect(steppingStone.posX - 500, steppingStone.posY, steppingStone.width, steppingStone.height);
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.closePath();
+}
+
+    function handleCollision(){
+        if(
+        (posX + 20 > steppingStone.posX &&
+        posX - 20 < steppingStone.posX + steppingStone.width &&
+        posY + 20 > steppingStone.posY &&
+        posY - 20 < steppingStone.posY + steppingStone.height) ||
+        (posX + 20 > steppingStone.posX - 500 &&
+        posX - 20 < steppingStone.posX - 500 + steppingStone.width &&
+        posY + 20 > steppingStone.posY &&
+        posY - 20 < steppingStone.posY + steppingStone.height)
+    ){
+        velocityY = -velocityY -0.5;
+    }
+}
+
+
+
+    
 
     function horizontalMovement(){
         posX += velocityX;
         
-        if(posX < 20 || posX > canvas.width - 20) {
-            velocityX *= -bounceFactor;
+        if(posX < 20) {
+            posX = 20;
+        } else if (posX > canvas.width - 20){
+            posX = canvas.width - 20;
+    }
+}
+    function handleKeyPress(e) {
+        switch (e.key) {
+            case "ArrowLeft":
+                velocityX = -horizontalSpeed;
+                break;
+            case "ArrowRight":
+                velocityX = horizontalSpeed;
+                break;
     }
 }
 
-    function animate(){
-        applyGravity();
-        horizontalMovement();
+    function handleKeyRelease(e) {
+        if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+            velocityX = 0;
+        }
+}
+
+    const animate = () => {
         drawBall();
+        horizontalMovement();
+        drawSteppingStone();
+        drawSteppingStone2();
         animationId = requestAnimationFrame(animate);
 }
 
     function startAnimation(){
         canvas.style.display = "block";
         playButton.style.display = "none";
+        document.addEventListener("keydown", handleKeyPress);
+        document.addEventListener("keyup", handleKeyRelease);
         animate();
 }
 
     function stopAnimation(){
         canvas.style.display = "none";
         playButton.style.display = "block";
+        document.removeEventListener("keydown", handleKeyPress);
+        document.removeEventListener("keyup", handleKeyRelease);
         cancelAnimationFrame(animationId);
 }
 
